e754628a21f94db5ae66bd712b2bffd63e01a226
junhyuk <dejavukk1@gmail.com>
2020-10-05 15:37:35 +0900

    Initial Commit

---
+++ SwiftUI_Demo2/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  SwiftUI_Demo2
+//
+//  Created by junhyuk on 2020/10/05.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
