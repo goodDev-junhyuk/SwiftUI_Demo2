1cad17e23d22a0f8f0ad473be26fa8bc3aa0a14d
junhyuk <dejavukk1@gmail.com>
2020-10-07 17:33:35 +0900

    alignment, spacer, padding 적용.

--- SwiftUI_Demo2/ContentView.swift
+++ SwiftUI_Demo2/ContentView.swift
@@ -9,13 +9,37 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        
+        VStack(alignment: .center, spacing: 15) {
+            Text("재정상태").font(.title)
+            
+            HStack(alignment: .top) {
+                Text("Q1 Sales").font(.headline)
+                Spacer()
+                VStack(alignment: .leading) {
+                    Text("1월")
+                    Text("2월")
+                    Text("3월")
+                
+                }
+                Spacer()
+                VStack(alignment: .leading) {
+                    Text("10000")
+                    Text("2000")
+                    Text("30000")
+                    
+                }
+                .padding(5)
+            }
+            .padding(5)
+        }
     }
 }
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
-        ContentView()
+            ContentView()
+                .previewDevice(PreviewDevice(rawValue: "iPhone XS"))
+                .previewDisplayName("iPhone XS")
     }
 }
